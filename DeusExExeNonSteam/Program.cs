94f22ec133efcc9e714b68f911501caa9ef98a76
CorpArmstrong <cherry.cake128@gmail.com>
2017-01-25 15:10:39 +0200

    Initial commit.

---
+++ DeusExExeNonSteam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DeusExExeNonSteam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LaunchDeusEx();
+        }
+
+        /// <summary>
+        /// Launch the legacy application with some options set.
+        /// </summary>
+        static void LaunchDeusEx()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "notepad.exe";
+            startInfo.Arguments = "";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            try
+            {
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
